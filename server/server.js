3db1cfad8332f2ab1eba21966f820a5278778385
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-07-20 10:12:50 +0200

    basic folder structure and working on the server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const clientPath = `../client`;
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+let port = 8080;
+
+server.listen(8080, () =>{
+    console.log("server running on "+port);
+});
+
+const io = require('socket.io')(server);
+
+let counter = 0;
+io.on('connection', (socket) => {
+
+    counter++;
+    console.log(counter+' someone connected');
+    socket.on('sendToAll', (message) =>{
+        io.emit("displayMessage", (message));
+    });
+});
+
+document.getElementById(`global`).addEventListener(`click`, () => {
+    socket.emit('sendToAll', ('message'));
+})
+
